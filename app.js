e94609405a722e380924d48464c7c3b9eaa2c809
mrcoderider <immohanbera7@gmail.com>
2021-09-28 19:19:50 +0530

    Home is added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hi");
+console.log("Hello");
+
+const app = Vue.createApp({
+    //data, function will be here
+    data(){
+        return {
+            name: "Mohan Bera",
+            names: ["mohan", "Bera", "Sourav", "Bera"]
+        }
+    }
+});
+
+app.mount('#app');
